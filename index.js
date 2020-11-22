4dcf5972da72e1cec9c6d44cd28eefec4f3144f6
huy8856 <thai.giahuy123@gmail.com>
2020-11-23 01:33:43 +0700

    Cart-and-quantity-function

--- index.js
+++ index.js
@@ -69,23 +69,28 @@ $("#blogs .owl-carousel").owlCarousel({
 let $qty_up = $(".qty .qty-up");
 let $qty_down = $(".qty .qty-down");
 let $input = $(".qty .qty_input")
-//click on qty up button
+
+
+// click on qty up button
 $qty_up.click(function(e){
-    if($input.val()>=1&& $input.val()<=9){
+    let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+    if($input.val() >= 1 && $input.val() <= 9){
         $input.val(function(i, oldval){
             return ++oldval;
-        })
+        });
     }
-})
+});
 
-//click on qty down button
-$qty_down.click(function(e){
-    if($input.val()>1&& $input.val()<=10){
+// click on qty down button
+   $qty_down.click(function(e){
+    let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+    if($input.val() > 1 && $input.val() <= 10){
         $input.val(function(i, oldval){
             return --oldval;
-        })
+        });
     }
-})
+});
+
 
 
 });
