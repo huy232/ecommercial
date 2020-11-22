a5536b56863447703f7579e0006a01e7ecbcaa3d
huy8856 <thai.giahuy123@gmail.com>
2020-11-22 19:17:48 +0700

    front-page-1

---
+++ index.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+
+    // Banner Owl Carousel
+$("#banner-area .owl-carousel").owlCarousel({
+    dots: true,
+    items: 1
+    });
+    // Top sale owl carousel
+$("#top-sale .owl-carousel").owlCarousel({
+    loop:true,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items: 1
+        },
+        600:{
+            items: 3
+        },
+        1000:{
+            items: 5
+        } 
+    }
+ });
+//Isotope filters
+var $grid = $(".grid").isotope({
+    itemSelector:'.grid-item',
+    layoutMode:'fitRows'
+});
+ //Filter items on button click
+ $(".button-group").on("click","button",function(){
+     var filterValue=$(this).attr('data-filter');
+     $grid.isotope({filter:filterValue});
+ })
+
+ // New phones owl carousel
+$("#new-phones .owl-carousel").owlCarousel({
+    loop:true,
+    nav:false,
+    dots:true,
+    responsive:{
+        0:{
+            items: 1
+        },
+        600:{
+            items: 3
+        },
+        1000:{
+            items: 5
+        } 
+    }
+ });
+
+});
