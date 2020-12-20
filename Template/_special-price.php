946f0560c66e8eeff2cc129ac556eb26e492e81f
Anh Le <lhanhfire1999@gmail.com>
2020-12-20 22:38:49 +0700

    Add-to-cart Feature

--- Template/_special-price.php
+++ Template/_special-price.php
@@ -3,6 +3,14 @@
     $unique = array_unique($brand);
     sort($unique);
     shuffle($product_shuffle);
+
+    // request method post
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+    if (isset($_POST['special_price_submit'])){
+      // call method addToCart
+      $Cart->addToCart($_POST['user_id'], $_POST['item_id']);
+      }
+  }
 ?>
         <!-- Special Price-->
         <section id="special-price">
@@ -36,7 +44,11 @@
                                     <div class="price py-2">
                                         <span><?php echo $item['item_price'] ?? '0'; ?> VND</span>
                                     </div>
-                                    <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
+                                    <form method="post">
+                                      <input type="hidden" name="item_id" value="<?php echo $item['item_id'] ?? '1'; ?>">
+                                      <input type="hidden" name="user_id" value="<?php echo 1; ?>">
+                                      <button type="submit" name="special_price_submit" class="btn btn-warning font-size-12">Add to Cart</button>
+                                    </form>
                                 </div>
                             </div>
     
--- Template/_top-sale.php
+++ Template/_top-sale.php
@@ -3,6 +3,14 @@
   $product_shuffle = $product->getData();
   shuffle($product_shuffle);
 
+  // request method post
+  if($_SERVER['REQUEST_METHOD'] == "POST"){
+    if (isset($_POST['top_sale_submit'])){
+      // call method addToCart
+      $Cart->addToCart($_POST['user_id'], $_POST['item_id']);
+      }
+  }
+
 ?>
 
         <section id="top-sale">
@@ -27,7 +35,11 @@
                                 <div class="price py-2">
                                     <span><?php echo $item['item_price'] ?? '0' ; ?> VND</span>
                                 </div>
-                                <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
+                                <form method="post">
+                                  <input type="hidden" name="item_id" value="<?php echo $item['item_id'] ?? '1'; ?>">
+                                  <input type="hidden" name="user_id" value="<?php echo 1; ?>">
+                                  <button type="submit" name="top_sale_submit" class="btn btn-warning font-size-12">Add to Cart</button>
+                                </form>
                             </div>
                         </div>
 
