5fa23fc7b0cc8f05b18f7e3ca008da504244d478
Anh Le <lhanhfire1999@gmail.com>
2020-12-26 10:34:13 +0700

    Add to wishlist

--- Template/_wishlist_template.php
+++ Template/_wishlist_template.php
@@ -4,6 +4,10 @@
         if (isset($_POST['delete-cart-submit'])){
             $deletedrecord = $Cart->deleteCart($_POST['item_id']);
         }
+
+        if (isset($_POST['cart-submit'])){
+          $Cart->saveForLater($_POST['item_id'],'cart','wishlist');
+        }
     }
 ?>
 
@@ -15,7 +19,7 @@
             <div class="row">
                 <div class="col-sm-9">
                 <?php
-                  foreach ($product->getData('cart') as $item) :
+                  foreach ($product->getData('wishlist') as $item) :
                     $cart = $product->getProduct($item['item_id']);
                     $subTotal[] = array_map(function ($item){
                 ?>
@@ -47,7 +51,11 @@
                                           <input type="hidden" value="<?php echo $item['item_id'] ?? 0; ?>" name="item_id">
                                           <button type="submit" name="delete-cart-submit" class="btn font-baloo text-danger pl-0 pr-3 border-right">Delete</button>
                                         </form>
-                                        <button type="submit" class="btn font-baloo text-danger">Add to Cart</button>
+                                        <form method="post">
+                                          <input type="hidden" value="<?php echo $item['item_id'] ?? 0; ?>" name="item_id">
+                                          <button type="submit" name="cart-submit" class="btn font-baloo text-danger">Add to Cart</button>
+                                        </form>
+                                        
                                     </div>
                                 <!-- !product qty -->
 
